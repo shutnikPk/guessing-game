7a9e77130d8f8311b3cfdaa6900d189e506dbe03
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2019-10-07 20:52:05 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+       let min;
+       let max;
+       let num;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
+      
+        
     }
 
     guess() {
-
+    let num = (this.max + this.min) / 2;
+    this.num = Math.round(num);
+    return this.num;
+    
     }
 
     lower() {
-
+        this.max=this.num;
+        this.guess();
     }
 
     greater() {
-
+       this.min=this.num;      
+        this.guess();
     }
 }
 
